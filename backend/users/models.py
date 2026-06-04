dabe4fefce24ac8fcf766252041290ea6d17f351
Jyothish846 <ckjyothishchandran@gmail.com>
2026-06-04 22:39:15 +0530

    feat: implement user profile model with vocal choices and notes

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class UserProfile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    VOCAL_TYPE_CHOICES = [
+        ('not_sure', 'Not Sure'),
+        ('bass', 'Bass'),
+        ('tenor', 'Tenor'),
+        ('alto', 'Alto'),
+        ('soprano', 'Soprano'),
+    ]
+    vocal_type=models.CharField(
+            max_length=20,
+            choices=VOCAL_TYPE_CHOICES,
+            default='not_sure',
+        )
+    lowest_note = models.IntegerField(null=True, blank=True)
+    highest_note = models.IntegerField(null=True, blank=True)
